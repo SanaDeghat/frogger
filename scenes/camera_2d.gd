b302a4621c940e3929ce0756a2152c16d7633527
Sana Deghat <Sana.Deghat@gmail.com>
2026-07-07 02:21:01 -0700

    basic movement sprites and camera movmnt

---
+++ scenes/camera_2d.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+@onready var character_body_2d: CharacterBody2D = $"../CharacterBody2D"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position.x=character_body_2d.position.x
+	position.y=character_body_2d.position.y
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#position.y=character_body_2d.position.y
+	position.y-=0.25
+	print(character_body_2d.position.y)
+	print(position.y)
+	if character_body_2d.position.y<position.y-100:
+			position.y=character_body_2d.position.y+100
---
+++ scripts/frog.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+const TILE_SIZE = 100
+const SPEED = 200.0
+
+var target_position: Vector2
+var moving = false
+@onready var sprites: AnimatedSprite2D = $AnimatedSprite2D
+func _ready():
+	target_position = global_position
+
+func _physics_process(delta):
+	if !moving:
+		var direction = Vector2.ZERO
+
+		if Input.is_action_just_pressed("ui_left"):
+			direction = Vector2.LEFT
+			sprites.play("jump")
+		elif Input.is_action_just_pressed("ui_right"):
+	
+			sprites.play("jump")
+			direction = Vector2.RIGHT
+		elif Input.is_action_just_pressed("ui_up"):
+			sprites.play("jump")
+			direction = Vector2.UP
+		elif Input.is_action_just_pressed("ui_down"):
+			direction = Vector2.DOWN
+			sprites.play("jump-back")
+		if direction != Vector2.ZERO:
+			 
+			sprites.flip_h=(direction!=Vector2(1,0))
+			target_position += direction * TILE_SIZE
+			moving = true
+
+	if moving:
+		global_position = global_position.move_toward(target_position, SPEED * delta)
+
+		if global_position.distance_to(target_position) < 1:
+			global_position = target_position
+			moving = false
